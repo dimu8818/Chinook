44f6b92ea5f71568237feca14759ed87d961c174
Dimuth Pemadasa <dimuth@softgallery.io>
2024-06-09 22:29:56 +0530

    Playlist features

---
+++ Chinook/Services/Interfaces/IPlaylistService.cs
@@ -0,0 +1,11 @@
+﻿using Chinook.ClientModels;
+
+namespace Chinook.Services.Interfaces {
+    public interface IPlaylistService {
+        Task LoadPlaylists(string userId);
+
+        List<Playlist> Playlists { get; }
+
+        event Action OnChange;
+    }
+}
---
+++ Chinook/Services/PlaylistService.cs
@@ -0,0 +1,42 @@
+﻿using Chinook.ClientModels;
+using Chinook.Exceptions;
+using Chinook.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace Chinook.Services {
+    public class PlaylistService : IPlaylistService {
+        private readonly IDbContextFactory<ChinookContext> _dbContextFactory;
+
+        public List<Playlist> Playlists { get; private set; } = new List<Playlist>();
+
+        public event Action OnChange;
+
+        public PlaylistService(IDbContextFactory<ChinookContext> dbContextFactory) {
+            _dbContextFactory = dbContextFactory;
+        }
+
+        public async Task LoadPlaylists(string userId) {
+            try {
+                using var dbContext = await _dbContextFactory.CreateDbContextAsync();
+                Playlists = await dbContext.Playlists
+                    .Where(p => p.UserPlaylists.Any(up => up.UserId == userId))
+                    .Select(p => new Playlist {
+                        PlaylistId = p.PlaylistId,
+                        Name = p.Name
+                    })
+                    .ToListAsync();
+            }
+            catch (DbException ex) {
+                throw new PlaylistServiceException("An error occurred while accessing the database.", ex);
+            }
+            catch (Exception ex) {
+                throw new PlaylistServiceException("An unexpected error occurred.", ex);
+            }
+
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
